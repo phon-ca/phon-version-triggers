73ec40803ab39164cf9ef6cab5213d709789e763
Greg Hedlund <greg.hedlund@gmail.com>
2021-02-16 14:13:02 -0330

    Fix reset font trigger

--- src/main/java/ca/phon/app/triggers/ResetFontTrigger.java
+++ src/main/java/ca/phon/app/triggers/ResetFontTrigger.java
@@ -1,5 +1,6 @@
 package ca.phon.app.triggers;
 
+import ca.phon.app.VersionInfo;
 import ca.phon.app.VersionTrigger;
 import ca.phon.app.log.LogUtil;
 import ca.phon.plugin.IPluginExtensionFactory;
@@ -9,14 +10,16 @@ import ca.phon.util.PrefHelper;
 
 public class ResetFontTrigger implements VersionTrigger, IPluginExtensionPoint<VersionTrigger> {
 
-	private final static String PREVIOUS_VERSION_CHECK = "<3.2.0";
+	private final static String PREVIOUS_VERSION_CHECK = "<3.2.1";
 
 	public final static String RESET_FONT_PROP = ResetFontTrigger.class.getName() + ".tempResetFlag";
 
 
 	@Override
 	public void versionChanged(String prevVersion, String currentVersion) {
-		if(!PrefHelper.getBoolean(RESET_FONT_PROP, false)) {
+		VersionInfo pv = new VersionInfo(prevVersion);
+
+		if(pv.check(PREVIOUS_VERSION_CHECK) && !PrefHelper.getBoolean(RESET_FONT_PROP, false)) {
 			LogUtil.info("Resetting all font preferences");
 			FontPreferences.resetAll();
 			PrefHelper.getUserPreferences().putBoolean(RESET_FONT_PROP, true);
